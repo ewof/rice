c25a1448e769a97faa9cc29f3227b9b3e2ac6ded
ewof <marroq@marroq.xyz>
2025-05-02 20:08:03 -0400

    librewolf.overrides.cfg

--- .config/firefox/larbs.js
+++ .config/firefox/larbs.js
@@ -2,52 +2,52 @@
 // desired. Any of these settings can be overridden by the user.
 
 // Disable the Twitter/R*ddit/Faceberg ads in the URL bar:
-user_pref("browser.urlbar.quicksuggest.enabled", false);
-user_pref("browser.urlbar.suggest.topsites", false); // [FF78+]
+defaultPref("browser.urlbar.quicksuggest.enabled", false);
+defaultPref("browser.urlbar.suggest.topsites", false); // [FF78+]
 
 // Do not suggest web history in the URL bar:
-user_pref("browser.urlbar.suggest.history", false);
+defaultPref("browser.urlbar.suggest.history", false);
 
 // Do not prefil forms:
-user_pref("signon.prefillForms", false);
+defaultPref("signon.prefillForms", false);
 
 // Do not autocomplete in the URL bar:
-user_pref("browser.urlbar.autoFill", false);
+defaultPref("browser.urlbar.autoFill", false);
 
 // Enable the addition of search keywords:
-user_pref("keyword.enabled", true);
+defaultPref("keyword.enabled", true);
 
 // Allow access to http (i.e. not https) sites:
-user_pref("dom.security.https_only_mode", false);
+defaultPref("dom.security.https_only_mode", false);
 
 // Keep cookies until expiration or user deletion:
-user_pref("network.cookie.lifetimePolicy", 0);
+defaultPref("network.cookie.lifetimePolicy", 0);
 
-user_pref("dom.webnotifications.serviceworker.enabled", false);
+defaultPref("dom.webnotifications.serviceworker.enabled", false);
 
 // Disable push notifications:
-user_pref("dom.push.enabled", false);
+defaultPref("dom.push.enabled", false);
 
 // Disable the pocket antifeature:
-user_pref("extensions.pocket.enabled", false);
+defaultPref("extensions.pocket.enabled", false);
 
 // Don't autodelete cookies on shutdown:
-user_pref("privacy.clearOnShutdown.cookies", false);
+defaultPref("privacy.clearOnShutdown.cookies", false);
 
 // Enable custom userChrome.js:
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+defaultPref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 
 // This could otherwise cause some issues on bank logins and other annoying sites:
-user_pref("network.http.referer.XOriginPolicy", 0);
+defaultPref("network.http.referer.XOriginPolicy", 0);
 
 // Disable Firefox sync and its menu entries
-user_pref("identity.fxaccounts.enabled", false);
+defaultPref("identity.fxaccounts.enabled", false);
 
 // Fix the issue where right mouse button instantly clicks
-user_pref("ui.context_menus.after_mouseup", true);
+defaultPref("ui.context_menus.after_mouseup", true);
 
 // i want muh heckin dark moderino
-user_pref("privacy.resistFingerprinting", false);
+defaultPref("privacy.resistFingerprinting", false);
 
 // shit is slow without it sometimes
-user_pref("webgl.disabled", false);
+defaultPref("webgl.disabled", false);
