532a9cf8d97cf19b297c0fca6b10ff877b56fa7a
ewof <marroq@marroq.xyz>
2026-08-23 21:28:48 -0400

    thunderbird

---
+++ .config/thunderbird/larbs.js
@@ -0,0 +1,6 @@
+user_pref("mailnews.start_page.enabled", false);
+
+user_pref("privacy.clearOnShutdown_v2.cookiesAndStorage", false);
+
+user_pref("calendar.timezone.useSystemTimezone", true);
+user_pref("calendar.timezone.local", "America/New_York");
